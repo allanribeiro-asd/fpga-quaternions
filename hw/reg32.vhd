96691d53ac11ee52ae7e0a9cfe3ffde0a871733a
Lucas Perin <87390813+LucasLeyser@users.noreply.github.com>
2022-12-18 11:02:19 -0300

    reg

--- hw/reg32.vhd
+++ hw/reg32.vhd
@@ -2,7 +2,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 ENTITY reg32 IS
 	PORT (
-		clock, resetn : IN STD_LOGIC;
+		clock, reset : IN STD_LOGIC;
 		WE : IN STD_LOGIC;
 		D : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 		Q : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) );
@@ -19,9 +19,9 @@ ARCHITECTURE Behavior OF reg32 IS
   --END COMPONENT;
 
 BEGIN
-PROCESS(resetn, clock)
+PROCESS(reset, clock)
 BEGIN
-	IF resetn = '1' THEN
+	IF resetn = '0' THEN
 		Q <= x"00000000";
 	elsif clock'EVENT AND clock = '1' then
 		if WE = '1' then
